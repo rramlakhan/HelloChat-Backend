1c82139f8df50e4f1758f3d6178cc840d21f6020
Ram Lakhan <r08ramlakhan@gmail.com>
2025-04-04 10:30:37 +0530

    initial commit

---
+++ src/test/java/com/helloChat/HelloChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
